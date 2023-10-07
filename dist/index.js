7a0d47dd027aa01815268b89e6dc32fd72df6b80
syedkashiftirmizi <79271879+syedkashiftirmizi@users.noreply.github.com>
2023-10-07 09:28:41 +0500

    Add files via upload

---
+++ dist/index.js
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let loop = true;
+while (loop) {
+    const ans = await inquirer.prompt([
+        {
+            type: "input",
+            name: "Todo",
+            message: "What you want to do: "
+        },
+        {
+            type: "confirm",
+            name: "addmore",
+            message: "Do You Have More To Add: ",
+            default: false
+        }
+    ]);
+    const { Todo, addmore } = ans;
+    console.log(ans);
+    loop = addmore;
+    if (Todo) {
+        todos.push(Todo);
+    }
+    else {
+        console.log("Kindly Add valid input.");
+    }
+}
+if (todos.length > 0) {
+    console.log("Your Todo List:");
+    todos.forEach((todo) => {
+        console.log(todo);
+    });
+}
+else {
+    console.log("No Todos Found");
+}
